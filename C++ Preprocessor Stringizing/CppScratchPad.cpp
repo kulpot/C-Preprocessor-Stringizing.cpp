f95f0738b78446cecaec2d353cfd4beeeb7e1cd2
Kulpot <kulpotwahu@gmail.com>
2024-07-10 00:23:08 +0800

    https://www.youtube.com/watch?v=wKWwBlzQuqY&list=PLRwVmtr-pp06gII17UNrzeCGMMHnCPdOk&index=3

--- C++ Preprocessor Stringizing/CppScratchPad.cpp
+++ C++ Preprocessor Stringizing/CppScratchPad.cpp
@@ -3,6 +3,8 @@
 using std::cout;
 using std::endl;
 
+//ref link:https://www.youtube.com/watch?v=wKWwBlzQuqY&list=PLRwVmtr-pp06gII17UNrzeCGMMHnCPdOk&index=3
+
 #define PRINT(a) cout << a << endl
 
 void main()
