19c5e9dbd99ccfcb34cfef9992920a00fce49403
Kulpot <kulpotwahu@gmail.com>
2024-07-11 14:25:40 +0800

    ...

--- C++ Preprocessor Stringizing/CppScratchPad.cpp
+++ C++ Preprocessor Stringizing/CppScratchPad.cpp
@@ -4,7 +4,7 @@ using std::cout;
 using std::endl;
 
 //C++ Preprocessor Stringizing   //ref link:https://www.youtube.com/watch?v=wKWwBlzQuqY&list=PLRwVmtr-pp06gII17UNrzeCGMMHnCPdOk&index=3
-//Stringizng operator is # which is like a "", ex. cout << "var" << endl; is same as cout << #var << endl in #define
+//Stringizng operator is # which is basically means rapid in "", ex. cout << "var" << endl; is same as cout << #var << endl in #define
 
 //#define PRINT(a) cout << a << endl		//output: 5
 #define PRINT(a) cout << #a << endl			//output: var
