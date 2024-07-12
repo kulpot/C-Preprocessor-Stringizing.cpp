ed05f6c0cafad973d8275da6da285fdd20f1451c
Kulpot <kulpotwahu@gmail.com>
2024-07-12 18:47:27 +0800

    ..

--- C++ Preprocessor Stringizing/CppScratchPad.cpp
+++ C++ Preprocessor Stringizing/CppScratchPad.cpp
@@ -17,5 +17,6 @@ void main()
 	PRINT(var);
 	//cout << "var" << endl;	//output: var  //Stringizng operator is # which is basically means rapid in "", ex. cout << "var" << endl; is same as cout << #var << endl in #define
 	PRINT(bar);
+	cout << "bar: " << bar << endl; 
 	cout << "bar" ": " << bar << endl;
 }
